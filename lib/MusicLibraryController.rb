b1afbaf944a86d7ba4d61588b58b1a71dccac42c
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 02:04:53 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -9,6 +9,8 @@ class MusicLibraryController
   def call 
     @input = gets.chomp 
     
+    puts "Welcome to your music library!"
+    "What would you like to do?"
     "To list all of your songs, enter 'list songs'."
     "To list all of the artists in your library, enter 'list artists'."
     "To list all of the genres in your library, enter 'list genres'."
