d7fa82b65ed41e0a5774a78eab425f9371db8cef
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 22:34:19 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -96,6 +96,5 @@ class MusicLibraryController
       song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
       puts "Playing #{song.name} by #{song.artist.name}" if song
     end
-      #puts "Playing #{song.name} by #{song.artist.name}" if song
   end 
 end
