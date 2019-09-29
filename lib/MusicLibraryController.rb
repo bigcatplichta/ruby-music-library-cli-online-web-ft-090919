0f360b2555e359a821e3a082a39c33fce7e12c1b
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 06:01:38 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -88,7 +88,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     list = list_songs
-    input = gets.chomp.to_i
+    input = gets.chomp
     
     binding.pry 
     if input.between?(1, (list.count))
