da5e582b94a1f1d92f20a0191089adff4df5e08c
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 01:42:43 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -13,7 +13,7 @@ class MusicLibraryController
     "To list all of your songs, enter 'list songs'."
     "To list all of the artists in your library, enter 'list artists'."
     "To list all of the genres in your library, enter 'list genres'."
-    
+    "To list all of the songs by a particular artist, enter 'list artist'."
     
     
     case
