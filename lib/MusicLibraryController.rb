612aab2eaa2efdca85dbabbd176200d7ed95bf72
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 02:19:05 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -13,7 +13,7 @@ class MusicLibraryController
     
     puts "Welcome to your music library!"
     puts "What would you like to do?"
-    "To list all of your songs, enter 'list songs'."
+    puts "To list all of your songs, enter 'list songs'."
     "To list all of the artists in your library, enter 'list artists'."
     "To list all of the genres in your library, enter 'list genres'."
     "To list all of the songs by a particular artist, enter 'list artist'."
