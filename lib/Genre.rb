102b1289497675b595a3924123f455cf921a9696
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 01:34:55 +0000

    Automatically backed up by Learn

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -1,29 +1,40 @@
-class Genre 
-  attr_accessor :name 
+class Genre
+​
+extend Concerns::Findable
+​
+  attr_accessor :name
   @@all = []
-  
+​
   def initialize(name)
-    @name= name
+    @name = name
     @songs = []
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def save 
-    @@all << self 
-  end 
-  
+    save
+  end
+​
+  def self.all
+    @@all
+  end
+​
   def self.destroy_all
-    @@all.clear 
-  end 
-  
-  def self.create(name) 
-    new_genre = Genre.new(name)
-    new_genre.save
-    new_genre
-  end 
-  
-  
-end 
+    @@all.clear
+  end
+​
+  def save
+      @@all << self
+  end
+​
+  def self.create(name)
+    Genre.new(name)
+  end
+​
+  def songs
+    @songs
+  end
+​
+  def artists
+      songs.collect {|song| song.artist}.uniq
+  end
+​
+​
+​
+end
