ef60d164ecec0dedb58b7913bb921d286a1a5285
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 02:23:28 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -21,23 +21,23 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     
-    while @input != "exit" do  
-      case @input 
-      when "list songs"
-        list_songs 
-      when "list artists"
-        list_artists
-      when "list genres"
-        list_genres
-      when "list artist"
-        list_songs_by_artist
-      when "list genre"
-        list_songs_by_genre
-      when "play song"
-        play_song
-      end 
+    # while @input != "exit" do  
+    #   case @input 
+    #   when "list songs"
+    #     list_songs 
+    #   when "list artists"
+    #     list_artists
+    #   when "list genres"
+    #     list_genres
+    #   when "list artist"
+    #     list_songs_by_artist
+    #   when "list genre"
+    #     list_songs_by_genre
+    #   when "play song"
+    #     play_song
+    #   end 
     
-    end 
+    # end 
   end 
     
     
