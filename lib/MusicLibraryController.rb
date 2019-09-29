cd43695eeb04b3631ba1a223ae9ab7ed6bf7055b
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 02:03:35 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -8,9 +8,7 @@ class MusicLibraryController
 
   def call 
     @input = gets.chomp 
-  
-    while @input != "exit" do 
-    "Welcome to your music library!"
+    
     "To list all of your songs, enter 'list songs'."
     "To list all of the artists in your library, enter 'list artists'."
     "To list all of the genres in your library, enter 'list genres'."
@@ -18,42 +16,55 @@ class MusicLibraryController
     "To list all of the songs of a particular genre, enter 'list genre'."
     "To play a song, enter 'play song'."
     "To quit, type 'exit'."
+    
+    
+    while @input != "exit" do 
+    "Welcome to your music library!"
     "What would you like to do?"
       
       case @input 
       when "list songs"
         list_songs 
-      when 
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end 
     
     end 
-    
-    def list_songs
-        Song.all.each {|song| puts song.name}
-    end 
-    
-    def list_artists
-      if @input = "list artists"
-        #list artists
-      end
-    end 
-    
-    def list_genres
-      
-    end
-    
-    def list_songs_by_artist 
-      
-    end 
-    
-    def list_songs_by_genre 
-      
-    end 
-    
-    def play_song
-      
-    end 
-    
-      
   end 
-
+    
+    
+  def list_songs
+      Song.all.each {|song| puts song.name}
+  end 
+  
+  def list_artists
+    if @input = "list artists"
+      #list artists
+    end
+  end 
+  
+  def list_genres
+    
+  end
+  
+  def list_songs_by_artist 
+    
+  end 
+  
+  def list_songs_by_genre 
+    
+  end 
+  
+  def play_song
+    
+  end 
+  
 end
