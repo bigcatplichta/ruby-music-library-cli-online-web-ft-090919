9ed1f1ff4e5e168ac5f05ea5506732496c9c76a8
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 01:46:14 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -10,6 +10,17 @@ class MusicLibraryController
     input = gets.chomp 
     
     "Welcome to your music library!"
+    # "To list all of your songs, enter 'list songs'."
+    # "To list all of the artists in your library, enter 'list artists'."
+    # "To list all of the genres in your library, enter 'list genres'."
+    # "To list all of the songs by a particular artist, enter 'list artist'."
+    # "To list all of the songs of a particular genre, enter 'list genre'."
+    # "To play a song, enter 'play song'."
+    # "To quit, type 'exit'."
+    # "What would you like to do?"
+    
+    
+    while input != "exit" do 
     "To list all of your songs, enter 'list songs'."
     "To list all of the artists in your library, enter 'list artists'."
     "To list all of the genres in your library, enter 'list genres'."
@@ -18,10 +29,7 @@ class MusicLibraryController
     "To play a song, enter 'play song'."
     "To quit, type 'exit'."
     "What would you like to do?"
-    
-    
-    
-      
+    end 
       
   end 
 
