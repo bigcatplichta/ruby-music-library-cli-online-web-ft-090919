3893665e8a0620d5ecd544378a15c66ccc7e5cc5
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 04:53:32 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -94,7 +94,7 @@ class MusicLibraryController
      if Genre.all.find { |x| x.name == input }
      genre = Genre.all.select { |x| x.name == input }
     # list = genre.songs.select {genre.name == input}
-     binding.pry
+     
      list.each.with_index(1) {|song, i| puts "#{i}. #{song.artist.name} - #{song.name}"}
      end
   end 
