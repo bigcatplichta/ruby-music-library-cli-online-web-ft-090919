19062e6b4a97874266c7962610e93ce0a18714ea
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 05:39:37 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -87,9 +87,10 @@ class MusicLibraryController
   
   def play_song
     puts "Which song number would you like to play?"
-    list_songs
+    list = list_songs
     input = gets.chomp
     selection = input.to_i-1
+    
     
     
     binding.pry
