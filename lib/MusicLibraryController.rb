894fcd468ae1349742627cb5de4239e149596a92
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 02:48:26 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
     
     
   def list_songs
-      Song.all.each.with_index(1) {|song, i|}
+      Song.all.each.with_index(1) {|song, i|  binding.pry}
       
       binding.pry
   end 
