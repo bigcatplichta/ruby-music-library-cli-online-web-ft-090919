a5090f47a97ad55233b3c3c6da5e0c0e9b5dd644
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 01:34:15 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,44 +1,66 @@
-class Song 
-  attr_accessor :name, :artist, :genre 
+class Song
+extend Concerns::Findable
+​
+  attr_accessor :name, :artist, :genre
   @@all = []
-  
-  def initialize(name, artist= nil, genre= nil)
-    @name= name
-    self.artist = artist 
-    @genre = genre 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def save 
-    @@all << self 
-  end 
-  
-  def destroy_all
-    @@all.clear 
-  end 
-  
-  def self.create(name, artist= nil, genre= nil) 
-    song = Song.new(name, artist, genre)
-    
-    song.save
-    song
-  end 
-  
-  def artist=(artist)
-    @artist.add_song(self) or @artist = Artist.new(artist_name) 
-    #@artist = Artist.new(artist) 
-  end 
-  
-  # def genre=(genre_name)
-  #   self.genre= genre_name
-  #   self.genre.save
-  # end 
-  
+​
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist !=nil
+    self.genre = genre if genre != nil
+    save
+  end
+​
+  def self.all
+    @@all
+  end
+​
   def self.destroy_all
     @@all.clear
-  end 
-  
-end 
+  end
+​
+  def save
+      @@all << self
+  end
+​
+  def self.create(name)
+    Song.new(name)
+  end
+​
+  def artist=(name)
+    @artist = name
+    name.add_song(self)
+  end
+​
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include? self
+  end
+​
+​
+​
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+​
+​
+  def self.new_from_filename(file_name)
+     artist = file_name.split(" - ")[0]
+     title = file_name.split(" - ")[1]
+     genre = file_name.split(" - ")[2].chomp(".mp3")
+​
+     song = Song.find_or_create_by_name(title)
+     artist = Artist.find_or_create_by_name(artist)
+     genre = Genre.find_or_create_by_name(genre)
+​
+     song.artist = artist
+     song.genre = genre
+     song
+   end
+​
+   def self.create_from_filename(filename)
+     new_from_filename(filename)
+   end
+​
+​
+end
